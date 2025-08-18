7d4c0785bccfad0aef1af023b8ee64b7b001df21
aldhussein <aldhusseinali@gmail.com>
2025-08-18 12:36:20 +0300

    some thing layout

--- app/components/storefront/Navbar.tsx
+++ app/components/storefront/Navbar.tsx
@@ -24,7 +24,7 @@ export async function Navbar() {
       <div className="flex items-center">
         <Link href="/">
           <h1 className="text-black font-bold text-xl lg:text-3xl">
-            Shoe<span className="text-primary">Marshal</span>
+            ALD<span className="text-primary">TECHs</span>
           </h1>
         </Link>
         <NavbarLinks />
