1f97076486cd9d088557936e59ff9c18592ac415
aldhussein <aldhusseinali@gmail.com>
2025-08-19 11:54:02 +0300

    menu

--- app/components/storefront/Navbar.tsx
+++ app/components/storefront/Navbar.tsx
@@ -31,7 +31,7 @@ export async function Navbar() {
           <NavbarLinks />
         </div>
 
-        <div className="flex items-center mr-5">
+        <div className="flex items-center mr-8">
           {user ? (
             <>
               <Link href="/bag" className="group p-2 flex items-center mr-2">
