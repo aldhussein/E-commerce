dab2e3a00a6876d207de618e13d4190c528e1b9e
aldhussein <aldhusseinali@gmail.com>
2025-08-19 10:06:28 +0300

    padding

--- app/(storefront)/bag/page.tsx
+++ app/(storefront)/bag/page.tsx
@@ -29,7 +29,7 @@ export default async function BagRoute() {
   });
 
   return (
-    <div className="max-w-2xl mx-auto mt-10 min-h-[55vh] mt-6">
+    <div className="max-w-2xl mx-auto mt-16 min-h-[55vh]">
       {!cart || !cart.items ? (
         <div className="flex min-h-[400px] flex-col items-center justify-center rounded-lg border border-dashed p-8 text-center mt-20">
           <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
