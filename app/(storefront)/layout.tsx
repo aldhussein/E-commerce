753c27911caf73715ec0354bcdad66c1cad7f841
aldhussein <aldhusseinali@gmail.com>
2025-08-19 09:30:54 +0300

    padding

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -10,7 +10,7 @@ export default function StoreFrontLayout({
   return (
     <>
       <Navbar />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10">{children}</main>
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-15">{children}</main>
       <Footer />
     </>
   );
