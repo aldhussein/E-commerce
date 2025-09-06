ecf1c4c0282c8d746109e36355c379795dcb67ad
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-09-06 15:22:01 +0300

    Fixing admins

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -30,7 +30,7 @@ export default async function DashboardLayout({
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  if (!user || !allowedEmails.includes(user.email as string)) {
+  if (!user || !user.roles.includes("admin")) {
   return redirect("/");
 }
   return (
